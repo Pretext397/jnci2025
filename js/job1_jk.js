fe31f04de4dad52f958adac03b6c075fff9903ca
Pretext397 <450746621@qq.com>
2025-03-27 19:45:37 +0800

    11

--- js/job1_jk.js
+++ js/job1_jk.js
@@ -3,8 +3,9 @@ var params = {
   pagesize: "",
   // poscode:"a"
 };
+var job = []
 function filterAndSortJobsByPrefix(prefix) {
-  const filteredJobs = jobarr.filter((job) =>
+  const filteredJobs = job.filter((job) =>
     job.poscode.startsWith(`${prefix}-`)
   );
   filteredJobs.sort((a, b) => {
@@ -20,7 +21,7 @@ function joblist(poscode) {
     //取到数据之后的操作
     console.log(data);
     var html = "";
-    var job = data.resultbody.joblist;
+    job = data.resultbody.joblist;
     if (job == undefined || job == []) {
       html =
         ' <div style="text-align:center;line-height: 100px;">暂无数据</div>';
@@ -28,21 +29,28 @@ function joblist(poscode) {
       console.log(poscode, "poscode2");
       const job = filterAndSortJobsByPrefix(poscode);
       console.log(job, "过滤数组");
-      for (k in job) {
-        html += `
-                <div class="jobs">
-                <div class="jobtit" jobid="${job[k].jobid}">
-                  <span>${job[k].jobname}</span>
-                  <span>查看详情<img src="./images/i.png" alt=""></span>
-                </div>
-                <div class="jobinfo">
-                  <div class="jobtxt">
-                  </div>
-                  <div class="jobbtn"> <a href=""><img src="./images/btn.png" alt=""></a></div>
-                </div>
-             </div>
-                `;
-      }
+      if (job == undefined  || job.length == 0) {
+        console.log('7777')
+          html =
+            ' <div style="text-align:center;line-height: 100px;">暂无数据</div>';
+        } else{
+          for (k in job) {
+            html += `
+                    <div class="jobs">
+                    <div class="jobtit" jobid="${job[k].jobid}">
+                      <span>${job[k].jobname}</span>
+                      <span>查看详情<img src="./images/i.png" alt=""></span>
+                    </div>
+                    <div class="jobinfo">
+                      <div class="jobtxt">
+                      </div>
+                      <div class="jobbtn"> <a href=""><img src="./images/btn.png" alt=""></a></div>
+                    </div>
+                 </div>
+                    `;
+          }
+        }
+     
     }
 
     $(".joblist").html(html);
@@ -66,18 +74,19 @@ $(document).on("click", ".jobtit", function () {
   let jobid = $(this).attr("jobid");
   var that = $(this);
   coapi.getJobDetail(jobid, function (data) {
+    console.log( data.resultbody,' data.resultbody')
     data.resultbody.jobinfo = data.resultbody.jobinfo.replace(
-      /(专业要求:|资格证书或其他要求:|招聘人数:|岗位职责:|专业要求：|资格证书或其他要求：|招聘人数：|岗位职责：)/gm,
+      /(专业要求:|资格证书或其他要求:|招聘人数:|岗位职责:|专业要求：|资格证书或其他要求：|招聘人数：|岗位职责：|岗位要求：|职位要求：)/gm,
       "<b>$1</b>"
     );
     var jobinfo = `
         <div class="jobinfobk">
          <p><b>学历要求：</b></p>
-         ${jobData.edu}
+         ${data.resultbody.degreefrom}
          <p><b>招聘人数：</b></p>
-         ${jobData.num}人
-        <div class="jobtxt">${jobData.res}</div>
-        <div class="jobbtn"><a href="http://xyz.51job.com/external/apply.aspx?jobid=${jobData.jobid}&ctmid=8454839"><img src="./images/btn.png" alt=""></a></div>
+         ${data.resultbody.jobnum}人
+        <div class="jobtxt">${data.resultbody.jobinfo}</div>
+        <div class="jobbtn"><a href="http://xyz.51job.com/external/apply.aspx?jobid=${data.resultbody.jobid}&ctmid=8454839"><img src="./images/btn.png" alt=""></a></div>
         </div>
         `;
     that.next().html(jobinfo);
